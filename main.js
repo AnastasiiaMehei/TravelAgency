35c0a46022b2e384ee24fb53f3f2286aa2b38059
AnastasiiaMehei <147993093+AnastasiiaMehei@users.noreply.github.com>
2024-08-14 19:18:32 +0300

    Modal for navigation, support, contacts

--- main.js
+++ main.js
@@ -71,3 +71,66 @@ document.addEventListener('DOMContentLoaded', function () {
     }
   };
 });
+
+// main.js
+document.addEventListener('DOMContentLoaded', function () {
+  // Modal for Навігація
+  var navigationModal = document.getElementById('navigationModal');
+  var openNavigationModalBtn = document.getElementById('openNavigationModal');
+  var closeNavigationModalBtn = navigationModal.querySelector('.close');
+
+  openNavigationModalBtn.onclick = function (event) {
+    event.preventDefault(); // Prevent default link behavior
+    navigationModal.style.display = 'block';
+  };
+
+  closeNavigationModalBtn.onclick = function () {
+    navigationModal.style.display = 'none';
+  };
+
+  window.onclick = function (event) {
+    if (event.target == navigationModal) {
+      navigationModal.style.display = 'none';
+    }
+  };
+
+  // Modal for Підтримка
+  var supportModal = document.getElementById('supportModal');
+  var openSupportModalBtn = document.getElementById('openSupportModal');
+  var closeSupportModalBtn = supportModal.querySelector('.close');
+
+  openSupportModalBtn.onclick = function (event) {
+    event.preventDefault(); // Prevent default link behavior
+    supportModal.style.display = 'block';
+  };
+
+  closeSupportModalBtn.onclick = function () {
+    supportModal.style.display = 'none';
+  };
+
+  window.onclick = function (event) {
+    if (event.target == supportModal) {
+      supportModal.style.display = 'none';
+    }
+  };
+
+  // Modal for Контакти
+  var contactsModal = document.getElementById('contactsModal');
+  var openContactsModalBtn = document.getElementById('openContactsModal');
+  var closeContactsModalBtn = contactsModal.querySelector('.close');
+
+  openContactsModalBtn.onclick = function (event) {
+    event.preventDefault(); // Prevent default link behavior
+    contactsModal.style.display = 'block';
+  };
+
+  closeContactsModalBtn.onclick = function () {
+    contactsModal.style.display = 'none';
+  };
+
+  window.onclick = function (event) {
+    if (event.target == contactsModal) {
+      contactsModal.style.display = 'none';
+    }
+  };
+});
