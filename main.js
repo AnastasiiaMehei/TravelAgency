61389bba33e8445a942de48cafd213b0f47ccb82
AnastasiiaMehei <147993093+AnastasiiaMehei@users.noreply.github.com>
2024-08-14 15:47:38 +0300

    modal

--- main.js
+++ main.js
@@ -29,23 +29,44 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 });
 // Modal
-document.addEventListener('DOMContentLoaded', function() {
-    var modal = document.getElementById('myModal');
-    var btn = document.getElementById("openModal");
-    var span = document.querySelector('.close');
+document.addEventListener('DOMContentLoaded', function () {
+  var modal = document.getElementById('myModal');
+  var btn = document.getElementById('openModal');
+  var span = document.querySelector('.close');
 
-    btn.onclick = function(event) {
-        event.preventDefault(); // Prevent default link behavior
-        modal.style.display = "block";
-    }
+  btn.onclick = function (event) {
+    event.preventDefault(); // Prevent default link behavior
+    modal.style.display = 'block';
+  };
 
-    span.onclick = function() {
-        modal.style.display = "none";
-    }
+  span.onclick = function () {
+    modal.style.display = 'none';
+  };
 
-    window.onclick = function(event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modal.style.display = 'none';
     }
+  };
+});
+// Модалка для замовлення
+document.addEventListener('DOMContentLoaded', function () {
+  var modal = document.getElementById('modalForServices');
+  var btn = document.getElementById('openServicesModal');
+  var span = document.querySelector('.close');
+
+  btn.onclick = function (event) {
+    event.preventDefault(); // Prevent default link behavior
+    modal.style.display = 'block';
+  };
+
+  span.onclick = function () {
+    modal.style.display = 'none';
+  };
+
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modal.style.display = 'none';
+    }
+  };
 });
