9ae9ebd097eac7ec716be2594d8eb6ca12e7b52b
AnastasiiaMehei <147993093+AnastasiiaMehei@users.noreply.github.com>
2024-08-14 20:06:05 +0300

    modals

--- main.js
+++ main.js
@@ -1,34 +1,3 @@
-document.addEventListener('DOMContentLoaded', function () {
-  var images = [
-    './images/banner1.jpeg',
-    './images/banner4.png',
-    './images/banner5.jpg',
-  ];
-  var currentIndex = 0;
-  var heroImage = document.getElementById('heroImage');
-  var nextIndex = (currentIndex + 1) % images.length;
-
-  document.getElementById('btn-img').addEventListener('click', function () {
-    var nextImage = document.createElement('img');
-    nextImage.src = images[nextIndex];
-    nextImage.style.opacity = '0';
-    nextImage.style.position = 'absolute';
-    nextImage.style.width = '100%';
-    // nextImage.style.height = 'auto';
-    nextImage.style.transition = 'opacity 1s ease-in-out';
-    document.querySelector('.section-hero').appendChild(nextImage);
-
-    setTimeout(function () {
-      heroImage.style.opacity = '0';
-      heroImage.src = images[nextIndex];
-      heroImage.style.opacity = '1';
-      nextImage.remove();
-      currentIndex = nextIndex;
-      nextIndex = (currentIndex + 1) % images.length;
-    }, 1000);
-  });
-});
-// Modal
 // main.js
 document.addEventListener('DOMContentLoaded', function () {
   // Modal for Тури
@@ -70,10 +39,7 @@ document.addEventListener('DOMContentLoaded', function () {
       modalForServices.style.display = 'none';
     }
   };
-});
 
-// main.js
-document.addEventListener('DOMContentLoaded', function () {
   // Modal for Навігація
   var navigationModal = document.getElementById('navigationModal');
   var openNavigationModalBtn = document.getElementById('openNavigationModal');
